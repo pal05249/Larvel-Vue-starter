870577376a326598ceda80b0404f9462fb9e2303
Gaurav Pal <pal40419083@gmail.com>
2021-04-06 19:41:58 +0530

    initial commit

---
+++ example-app/resources/js/components/Index.vue
@@ -0,0 +1,30 @@
+
+<template>
+	<div>
+		<nav class="navbar navbar-light" style="background-color: #e3f2fd">
+			<router-link class="navbar-brand mr-auto" to="/dashboard"
+				>Dashboard</router-link
+			>
+			<button class="btn" @click="logout">Logout</button>
+		</nav>
+		<div class="m-4 pr-4 pl-4"><router-view></router-view></div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	methods: {
+		logout() {
+			axios.post("/logout").then((response) => {
+				window.location.href = "/login";
+			});
+		},
+	},
+};
+</script>
+
+<style>
+</style>
